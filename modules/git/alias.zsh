beade4c0035a4e9ca5450c41ecc2d20d668a1df6
yqrashawn <namy.19@gmail.com>
2017-10-11 16:16:26 +0800

    gwss ( magit-status )

--- modules/git/alias.zsh
+++ modules/git/alias.zsh
@@ -252,6 +252,7 @@ if ! zstyle -t ':prezto:module:git:alias' skip 'yes'; then
 
   # Working Copy (w)
   alias gws='git status --ignore-submodules=${_git_status_ignore_submodules} --short'
+  alias gwss='emacsclient -e "(magit-status)"'
   alias gwS='git status --ignore-submodules=${_git_status_ignore_submodules}'
   alias gwd='git diff --no-ext-diff'
   alias gwD='git diff --no-ext-diff --word-diff'
